e173a730cee85993f1f22b27ec7200c49822a346
csand <sam@csand.net>
2014-06-08 16:59:21 -0400

    Remove ember-data from build

--- Brocfile.js
+++ Brocfile.js
@@ -13,9 +13,6 @@ var app = new EmberApp({
   getEnvJSON: require('./config/environment')
 });
 
-// Use this to add additional libraries to the generated output files.
-app.import('vendor/ember-data/ember-data.js');
-
 // If the library that you are including contains AMD or ES6 modules that
 // you would like to import into your application please specify an
 // object with the list of modules as keys along with the exports of each
